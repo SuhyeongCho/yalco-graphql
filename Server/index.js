1944ebbdb7ef0c574dfcdbafb8dca2b9c324c275
SuhyeongCho <ch5951130@naver.com>
2021-01-19 17:15:36 +0900

    supply 타입 추가해보기

--- Server/index.js
+++ Server/index.js
@@ -3,10 +3,11 @@ const { ApolloServer } = require("apollo-server");
 const queries = require("./typedefs-resolvers/_queries");
 const mutations = require("./typedefs-resolvers/_mutations");
 const equipments = require("./typedefs-resolvers/equipments");
+const supplies = require("./typedefs-resolvers/supplies");
 
-const typeDefs = [queries, mutations, equipments.typeDefs];
+const typeDefs = [queries, mutations, equipments.typeDefs, supplies.typeDefs];
 
-const resolvers = [equipments.resolvers];
+const resolvers = [equipments.resolvers, supplies.resolvers];
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
